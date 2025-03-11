d003678b61f6501c02978b4b814fadc66a0b19fc
fray osorio <frayosorio@gmail.com>
2025-03-10 21:31:50 -0500

    Se agrega funcionalidad para actualizar la muestra y calcular la sumatoria

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -15,6 +16,11 @@ import javax.swing.WindowConstants;
 
 public class FrmEstadistica extends JFrame {
 
+    private JTextField txtDato;
+    private JList lstMuestra;
+    private JComboBox cmbEstadistica;
+    private JTextField txtEstadistica;
+
     // metodo constructor
     public FrmEstadistica() {
         setSize(500, 300);
@@ -26,7 +32,7 @@ public class FrmEstadistica extends JFrame {
         lblDato.setBounds(10, 10, 100, 25);
         getContentPane().add(lblDato);
 
-        JTextField txtDato = new JTextField();
+        txtDato = new JTextField();
         txtDato.setBounds(110, 10, 100, 25);
         getContentPane().add(txtDato);
 
@@ -43,7 +49,7 @@ public class FrmEstadistica extends JFrame {
         lblMuestra.setHorizontalAlignment(SwingConstants.CENTER);
         getContentPane().add(lblMuestra);
 
-        JList lstMuestra = new JList();
+        lstMuestra = new JList();
         JScrollPane spMuestra = new JScrollPane(lstMuestra);
         spMuestra.setBounds(220, 40, 100, 150);
         getContentPane().add(spMuestra);
@@ -52,45 +58,98 @@ public class FrmEstadistica extends JFrame {
         btnEstadistica.setBounds(10, 200, 100, 25);
         getContentPane().add(btnEstadistica);
 
-        JComboBox cmbEstadistica = new JComboBox();
+        cmbEstadistica = new JComboBox();
         String[] opciones = new String[] { "Sunatoria", "Promedio", "Desviación Estandar", "Máximo", "Mínimo", "Moda" };
         DefaultComboBoxModel mdlEstadistica = new DefaultComboBoxModel(opciones);
         cmbEstadistica.setModel(mdlEstadistica);
         cmbEstadistica.setBounds(110, 200, 100, 25);
         getContentPane().add(cmbEstadistica);
 
-        JTextField txtEstadistica = new JTextField();
+        txtEstadistica = new JTextField();
         txtEstadistica.setBounds(220, 200, 100, 25);
         txtEstadistica.setEnabled(false);
         getContentPane().add(txtEstadistica);
 
         // Agregar eventos
-        btnAgregar.addActionListener(new ActionListener(){
+        btnAgregar.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hizo clic en AGREGAR");
+                agregarDato();
             }
-            
+
         });
 
-        btnQuitar.addActionListener(new ActionListener(){
+        btnQuitar.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hizo clic en QUITAR");
+                quitarDato();
             }
-            
+
         });
 
-        btnEstadistica.addActionListener(new ActionListener(){
+        btnEstadistica.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hizo clic en ESTADISTICA");
+                calcularEstadistica();
             }
-            
+
         });
+    }
+
+    private int MAXIMO_DATOS = 1000;
+
+    private double[] muestra = new double[MAXIMO_DATOS];
+    private int totalDatos = 0;
+
+    private void agregarDato() {
+        if (totalDatos <= MAXIMO_DATOS) {
+            muestra[totalDatos] = Double.parseDouble(txtDato.getText());
+            totalDatos++;
+            mostrar();
+        } else {
+            JOptionPane.showMessageDialog(null, "No se pueden agregar mas datos");
+        }
+    }
+
+    private void mostrar() {
+        String[] strMuestra = new String[totalDatos];
+        for (int i = 0; i < totalDatos; i++) {
+            strMuestra[i] = String.valueOf(muestra[i]);
+        }
+        lstMuestra.setListData(strMuestra);
+    }
+
+    private void quitarDato() {
+        int posicion = lstMuestra.getSelectedIndex();
+        if (posicion >= 0) {
+            for (int i = posicion; i < totalDatos - 1; i++) {
+                muestra[i] = muestra[i + 1];
+            }
+            totalDatos--;
+            mostrar();
+        } else {
+            JOptionPane.showMessageDialog(null, "debe seleccionar la posición a retirar");
+        }
+    }
+
+    private double sumatoria() {
+        double suma = 0;
+        for (int i = 0; i < totalDatos; i++) {
+            suma += muestra[i];
+        }
+        return suma;
+    }
+
+    private void calcularEstadistica() {
+        switch (cmbEstadistica.getSelectedIndex()) {
+            case 0:
+                txtEstadistica.setText(String.valueOf(sumatoria()));
+                break;
+
+        }
     }
 
 }
